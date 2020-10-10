e190fe0caf69ed1f8c4bb19176177ec1be274aa5
Diogo Amaral <jda@isep.ipp.pt>
2020-10-10 12:32:28 +0100

    First Commit

---
+++ Backend/Controllers/CustomersController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomersController : ControllerBase
+    {
+        private List<Customers> Customers = new List<Customers>();
+
+
+        private readonly ILogger<CustomersController> _logger;
+
+        public CustomersController(ILogger<CustomersController> logger)
+        {
+            _logger = logger;
+            LoadData();
+
+        }
+
+        private void LoadData()
+        {
+            Customers.Add(new Customers { Id = 1, Name = "Isaac Garrison", RegisterDate = Convert.ToDateTime("01-12-2019"), Status = true });
+            Customers.Add(new Customers { Id = 2, Name = "Elisabeth Mcgrath", RegisterDate = Convert.ToDateTime("05-20-2020"), Status = true });
+            Customers.Add(new Customers { Id = 3, Name = "Ariana Hughes", RegisterDate = Convert.ToDateTime("01-12-2019"), Status = true });
+        }
+
+        [HttpGet]
+        public IEnumerable<Customers> Get()
+        {
+
+            return Customers;
+        }
+    }
+}
---
+++ Backend/Models/Customers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Backend.Models
+{
+    public class Customers
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime RegisterDate { get; set; }
+        public bool Status { get; set; }
+    }
+}
